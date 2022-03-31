f18f2abe3eb7e87b06c0a1b92981d4f6652c99b5
RubikRr <vova.chakalov.2003@gmail.com>
2022-03-31 12:42:52 +0300

    Add project files.

---
+++ Ships/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships
+{
+    enum Ships
+    {
+        Banana,
+        Clothe,
+        Bread
+    }
+    enum Sizes
+    {
+        Small = 10,
+        Average = 50,
+        Huge = 100
+    }
+}
---
+++ Ships/Ship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships
+{
+    class Ship
+    {
+        public int count { get; private set; }
+        public Ships type { get; }
+        public Sizes size { get; }
+
+        public Ship(Ships typeShip, Sizes sizeShip)
+        {
+            type = typeShip;
+            size = sizeShip;
+        }
+
+        public void Add(int count)
+        {
+            this.count += count;
+        }
+
+        public bool CheckCount()
+        {
+            return count <= (int)size;
+        }
+    }
+}
